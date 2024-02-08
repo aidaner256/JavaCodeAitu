8e0e38075f22ffe82faf5e1ace192a334473e6a5
aidaner256 <aidan.yermekbayev@gmail.com>
2024-02-08 23:43:19 +0600

    window

---
+++ CreateWindow.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+ 
+public class CreateWindow{
+  public static void main(String[] args){
+    JFrame frame = new JFrame();
+    frame.setSize(1080,720);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setTitle("Kaspi Dev");
+    frame.setVisible(true);
+    
+  }
+}
